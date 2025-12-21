a16c68086b7a8ce4cfe35cbf6433bdebae823aeb
Rajendra <rajendraitchapurapu@gmail.com>
2025-12-21 17:56:50 +0530

    Removed Desktop class from the sample.py file.

--- sample.py
+++ sample.py
@@ -8,33 +8,6 @@ class Smartphone:
     def description(self):
         return f"{self.device} of {self.brand} supports Android 14"
 
-
 # creating object of the class
 phoneObj = Smartphone("Smartphone", "Neo one")
 print(phoneObj.description())
-# This is a comment just to test git reset.
-
-
-class Desktop:
-    def __init__(self):
-        self.__max_price = 25000
-
-    def sell(self):
-        return f"Selling Price: {self.__max_price}"
-
-    def set_max_price(self, price):
-        if price > self.__max_price:
-            self.__max_price = price
-
-
-# Object
-desktopObj = Desktop()
-print(desktopObj.sell())
-
-# modifying the price directly
-desktopObj.__max_price = 35000
-print(desktopObj.sell())
-
-# modifying the price using setter function
-desktopObj.set_max_price(35000)
-print(desktopObj.sell())
