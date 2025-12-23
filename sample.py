dba6b3268b02c66e58d2c6f88e6c0726c392cc9f
Rajendra <rajendraitchapurapu@gmail.com>
2025-12-23 21:05:28 +0530

    modified the file with str functions

--- sample.py
+++ sample.py
@@ -1,13 +1,2 @@
-class Smartphone:
-    # constructor
-    def __init__(self, device, brand):
-        self.device = device
-        self.brand = brand
-
-    # method of the class
-    def description(self):
-        return f"{self.device} of {self.brand} supports Android 14"
-
-# creating object of the class
-phoneObj = Smartphone("Smartphone", "Neo one")
-print(phoneObj.description())
+str = "Apple"
+print(str.find("l"))
